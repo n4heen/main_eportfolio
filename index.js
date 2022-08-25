941adc942de54f0208164fc47e69d32ba369847a
Naheen Habib <naheenhabib@outlook.com>
2022-08-25 23:56:48 +1000

    new commit

--- index.js
+++ index.js
@@ -2,21 +2,26 @@
 //service_u5fxijb
 //ypzgOSunxrv_S4tiD
 
-async function contact(event) {
-
-    event.preventDefault()
-    //    await emailjs
-    //         .sendForm(
-    //             'service_u5fxijb',
-    //             'template_lhwmh29',
-    //             event.target,
-    //             ' ypzgOSunxrv_S4tiD'
-    //         )
-
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList+=" modal__overlay--visible"
-    setTimeout(()=>{
-        console.log('It worked')
-    },500)
-}
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_u5fxijb",
+        "template_lhwmh29",
+        event.target,
+        "ypzgOSunxrv_S4tiD"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+      });
+  }
