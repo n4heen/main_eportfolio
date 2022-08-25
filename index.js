8d1cf0f4a46b6f9aa89847f8d0eaf1f0997c7a89
Naheen Habib <naheenhabib@outlook.com>
2022-08-25 23:31:41 +1000

    new commit

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 //service_u5fxijb
 //ypzgOSunxrv_S4tiD
 
-function contact(){
-    console.log('it worked')
+function contact(event) {
+
+    event.preventDefault()
+    emailjs
+        .sendForm(
+            'service_u5fxijb',
+            'template_lhwmh29',
+            event.target,
+            ' ypzgOSunxrv_S4tiD'
+        )
 }
